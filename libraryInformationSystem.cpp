7a4bbc1341f0a0835094fe2f4878b1839b8c5f8c
janelle-lr <270065505@yoobeestudent.ac.nz>
2021-10-14 15:06:54 +1300

    first commit

---
+++ libraryInformationSystem.cpp
@@ -0,0 +1,12 @@
+// libraryInformationSystem.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Janelle's code
+    cout << "Hello World!\n";
+}
+
